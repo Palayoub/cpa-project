263e4ea2987722c0a7bd67da6e0b1b421484c380
the1ayoub <the1ayoub@gmail.com>
2022-08-02 23:19:02 +0100

    fix arch

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                OUR CPA PROJECT.
+            </header>
+        </div>
+    );
+}
+
+export default Home;
